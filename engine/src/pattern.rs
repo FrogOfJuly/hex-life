e1477ded0d53d4cf898e79337b6224017d638f5f
Kirill Golubev <frogofjuly@Me.local>
2023-08-14 01:39:03 +0300

    testing pattern translating

--- engine/src/pattern.rs
+++ engine/src/pattern.rs
@@ -1,4 +1,6 @@
-use std::{collections::HashMap, vec};
+use std::{collections::HashMap, str::FromStr, vec};
+
+use h3o::CellIndex;
 
 // pub struct Pattern {
 //     resolution: h3o::Resolution,
@@ -28,6 +30,51 @@ impl Pattern for Star {
             .filter(|idx| idx != center)
             .collect()
     }
+}
+
+struct SmallPulsar {
+    pattern: [CellIndex; 3],
+}
+
+impl SmallPulsar {
+    fn new() -> Self {
+        Self {
+            pattern: [
+                h3o::CellIndex::try_from(0x8201a7fffffffff).expect("Invalid literal for cell"),
+                h3o::CellIndex::try_from(0x8201affffffffff).expect("Invalid literal for cell"),
+                h3o::CellIndex::try_from(0x820117fffffffff).expect("Invalid literal for cell"),
+            ],
+        }
+    }
+}
+
+impl Pattern for SmallPulsar {
+    fn as_cells(&self, center: &h3o::CellIndex) -> Vec<h3o::CellIndex> {
+        transpose_pattern(self.pattern[0], *center, &self.pattern)
+    }
+}
+
+fn transpose_pattern(src: CellIndex, dst: CellIndex, pattern: &[CellIndex]) -> Vec<CellIndex> {
+    // Compute translation offset.
+    let src_coord = src.to_local_ij(src).expect("src coord");
+    let dst_coord = dst.to_local_ij(dst).expect("dst coord");
+    let i_offset = dst_coord.i() - src_coord.i();
+    let j_offset = dst_coord.j() - src_coord.j();
+
+    // Transpose the pattern from src to dst.
+    pattern
+        .iter()
+        .copied()
+        .map(|cell| {
+            // Compute the local IJ coordinate wrt original center cell.
+            let src_ij = cell.to_local_ij(src).expect("local IJ");
+            // Apply translation and re-anchor at destination center cell.
+            let dst_ij =
+                h3o::LocalIJ::new_unchecked(dst, src_ij.i() + i_offset, src_ij.j() + j_offset);
+            // Convert back to cell index.
+            CellIndex::try_from(dst_ij).expect("dst cell")
+        })
+        .collect::<Vec<_>>()
 }
 
 // impl Pattern {
@@ -127,7 +174,7 @@ pub fn create_pattern_map() -> HashMap<&'static str, Box<dyn Pattern>> {
 
     patterns.insert("Single cell", Box::new(SingleCell));
     patterns.insert("Star", Box::new(Star));
-    // patterns.insert("Large flicker", Box::new(Pattern::large_flicker));
+    patterns.insert("Small pulsar", Box::new(SmallPulsar::new()));
     // patterns.insert("Rotating trio", Box::new(Pattern::rotating_trio));
     // patterns.insert("Pulsating trio", Box::new(Pattern::pulsating_trio));
     // patterns.insert("Blob", Box::new(Pattern::blob));
